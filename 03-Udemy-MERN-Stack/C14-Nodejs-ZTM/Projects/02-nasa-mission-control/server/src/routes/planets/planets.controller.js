2793a5af84519b51458f410ea8b0b4c47d81a7f1
salix-safsaf <114659600+salix-safsaf@users.noreply.github.com>
2024-12-10 07:08:51 +0200

    Delete 03-Udemy-MERN-Stack/C14-Nodejs-ZTM/Projects/02-nasa-mission-control directory
    nasa-mission-control directory Deleted NH

--- 03-Udemy-MERN-Stack/C14-Nodejs-ZTM/Projects/02-nasa-mission-control/server/src/routes/planets/planets.controller.js
+++
@@ -1,9 +0,0 @@
-const { getAllPlanets } = require("../../models/planets.model");
-
-function httpGetAllPlanets(req, res) {
-  return res.status(200).json(getAllPlanets());
-}
-
-module.exports = {
-  httpGetAllPlanets,
-};
